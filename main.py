8190b00294a174bb1a89c09f33abe5426b2d54d6
José A. Galindo <jagalindo@us.es>
2021-01-13 17:55:52 +0100

    Adding a main

---
+++ main.py
@@ -0,0 +1,32 @@
+from core.frozen_points_domain import *
+from core.frozen_points_plugins import DiscoverMetamodels
+
+# Creamos el manager y lo inicializamos
+dm = DiscoverMetamodels()
+#Buscamos los plugins disponibles
+available_plugins = dm.discover()
+
+#Creamos un modelo de manera programatica
+feature_b = Feature('B', [])
+relation = Relation(parent=None, children=[feature_b], card_min=0, card_max=1)	
+feature_a = Feature('A', [relation])	
+relation.parent = feature_a
+fm = FeatureModel(feature_a)
+
+#Imprimimos ese modelo
+print(fm)
+
+#Imprimimos los plugins disponibles
+print(available_plugins.get_plugin_names())
+
+#Buscamos el plugin que acabamos de crear
+plugin=available_plugins.get_plugin_by_name('count_leafs')
+
+#Imprimimos las operaciones de los plugins
+print(plugin.operations)
+
+#Usamos la operacion CountLeafs
+result=plugin.use_operation('CountLeafs',fm)
+
+#Imprimimos el resultado
+print("El modelo tiene " + result + "Features hojas")
